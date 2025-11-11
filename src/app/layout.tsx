9f4551d922346dae1b77669160a6f892a001e12c
= <=>
2025-11-11 13:54:58 +0530

    added analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import AuthProvider from "@/components/AuthProvider";
 import AuthAdminProvider from "@/components/AuthAdminProvider";
 import NavbarWrapper from "@/components/NavbarWrapper";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata: Metadata = {
   title: "Vibrant Flights-Elevate your jouney",
   description: "Vibrant Flights clothing store",
@@ -16,6 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased bg-neutral-900`}>
+        <Analytics/>
+        <SpeedInsights />
         <AuthAdminProvider>
           <AuthProvider>
             <NavbarWrapper/>
