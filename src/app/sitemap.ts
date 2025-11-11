fc807e5b9c51b7574e83e4e457fb948e5cd27997
= <=>
2025-11-11 15:53:07 +0530

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,43 +1,51 @@
-// import { ItemView } from "@/utils/items/itemView";
+import { ItemView } from "@/utils/items/itemView";
 
-// export default async function sitemap() {
-//   const baseUrl = "https://vibrantflight.in";
-//   const products = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/get-items`).then(r => r.json());
-
-//   const productUrls = products.map((p:ItemView) => ({
-//     url: `${baseUrl}/products/${p.itemId}`,
-//     lastModified: new Date(),
-//   }));
-
-//   return [
-//     {
-//       url: baseUrl,
-//       lastModified: new Date(),
-//     },
-//     {
-//         url: baseUrl+"/about-us",
-//         lastModified: new Date(),
-//     },
-//     {
-//         url: baseUrl+"/return-or-cancellation",
-//         lastModified: new Date(),
-//     },
-//     {
-//         url: baseUrl+"/users/login",
-//         lastModified: new Date(),
-//     },
-//     {
-//         url: baseUrl+"/users/register",
-//         lastModified: new Date(),
-//     },
-//     ...productUrls,
-//   ];
-// }
 export default async function sitemap() {
-    return [
-        {
-            url: "https://vibrantflight.in",
-            lastModified: new Date(),
-        }
-    ]
+  const baseUrl = "https://vibrantflight.in";
+  const products = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/get-items`).then(r => r.json());
+
+  const productUrls = products.map((p:ItemView) => ({
+    url: `${baseUrl}/products/${p.itemId}`,
+    lastModified: new Date(),
+  }));
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: baseUrl + "/products?category=tshirts",
+      lastModified: new Date(),
+    },
+    {
+      url: baseUrl + "/products?category=shirts",
+      lastModified: new Date(),
+    },
+    {
+      url: baseUrl + "/products?category=sweatshirts",
+      lastModified: new Date(),
+    },
+    {
+      url: baseUrl + "/products?category=overtees",
+      lastModified: new Date(),
+    },
+    {
+        url: baseUrl+"/about-us",
+        lastModified: new Date(),
+    },
+    {
+        url: baseUrl+"/return-or-cancellation",
+        lastModified: new Date(),
+    },
+    {
+        url: baseUrl+"/users/login",
+        lastModified: new Date(),
+    },
+    {
+        url: baseUrl+"/users/register",
+        lastModified: new Date(),
+    },
+    ...productUrls,
+  ];
 }
